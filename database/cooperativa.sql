5521a8b49b91ccd0b41f2b35dfc1ee065c78ef42
Francisco Hernandez <mihaelkyeah@gmail.com>
2021-05-25 20:42:12 -0300

    Base de datos actualizada
    Agregado columnas a la tabla pedido_recoleccion, agregado clave foránea referenciando a tabla material

--- database/cooperativa.sql
+++ database/cooperativa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-05-2021 a las 01:29:49
+-- Tiempo de generación: 26-05-2021 a las 01:37:02
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
