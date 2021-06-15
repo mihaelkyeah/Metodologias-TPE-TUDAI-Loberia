f87301c3a932e649ce2f495576e0d7acc690103a
Facundo Arana <facundoaranaloberia@gmail.com>
2021-06-15 18:13:07 -0300

    añadido formulario de ingreso de material

--- view/StorageView.php
+++ view/StorageView.php
@@ -5,11 +5,12 @@ require_once ('libs/smarty/Smarty.class.php');
 
 class StorageView extends View{
 
-    public function showStorageFormView($collectors) {
-        var_dump($collectors);
-        die();
+    public function showStorageFormView($collectors,$error=null, $success=null) {
         $this->getSmarty()->assign('title', 'storage');
+        $this->getSmarty()->assign('error', $error);
+        $this->getSmarty()->assign('success', $success);
         $this->getSmarty()->assign('collectors', $collectors);
-        $this->getSmarty()->display('templates/.tpl');  //crear tpl
+        $this->getSmarty()->display('templates/formMaterialStorage.tpl');  //crear tpl
     }
+
 }
